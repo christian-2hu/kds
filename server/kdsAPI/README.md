4d94ae44c5b3946bcbb05c69950f0134e47adc91
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-09-28 19:06:20 -0300

    Start project with spring boot

---
+++ server/kdsAPI/README.md
@@ -0,0 +1,3 @@
+# KDS API
+
+RESTful web service that handles the logic behind the kds
---
+++ server/kdsAPI/src/test/java/com/kdsAPI/KdsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
