82c14d930f225870db7e5637cec422b33ccea951
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-02 19:10:55 -0300

    Remove unwanted behavior on FoodItem
    FoodItem is just an item, it's not supposed to track the quantity wanted for the client, it is just an item that exists to represent what is on the menu

--- server/kdsAPI/src/main/java/com/kdsAPI/item/ItemFactory.java
+++ server/kdsAPI/src/main/java/com/kdsAPI/item/ItemFactory.java
@@ -1,11 +1,9 @@
 package com.kdsAPI.item;
 
-import java.util.Map;
-
 import com.kdsAPI.dto.item.ItemDTO;
 
 public class ItemFactory {
-    public Item createItem(Long id, Map<String, Integer> item) {
+    public Item createItem(Long id, String item) {
         return new ItemDTO(id, item);
     }
 
