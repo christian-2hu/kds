3cd10779cd35942951cad031fe67200989ec6714
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-05 18:48:51 -0300

    Transform the order interface into an abstract class
    The DTO and the DAO are almost the same thing, ignoring some annotations, instead of implementing it two times, we will use an abstract class that writes it once

--- server/kdsAPI/src/main/java/com/kdsAPI/order/Order.java
+++ server/kdsAPI/src/main/java/com/kdsAPI/order/Order.java
@@ -1,14 +1,76 @@
 package com.kdsAPI.order;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.kdsAPI.models.FoodItem;
+import com.kdsAPI.models.FoodOrder;
 
-public interface Order {
-    public OrderStatus getFoodOrderStatus();
-    public void setFoodOrderStatus(OrderStatus orderStatus);
-    public List<FoodItem> getFoodItem();
-    public void setFoodItem(List<FoodItem> foodItem);
-    public void addFoodItem(FoodItem foodItem);
-    public void deleteFoodItem(FoodItem foodItem);
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+    @ManyToMany
+    @NotNull(message = "\"This field cannot be empty\"")
+    protected List<FoodItem> foodItem;
+    @ElementCollection
+    @NotNull(message = "\"This field cannot be empty\"")
+    protected List<Integer> quantity;
+    protected OrderStatus foodOrderStatus;
+
+    public Order() {
+        this.foodOrderStatus = OrderStatus.WAITING;
+        this.foodItem = new LinkedList<>();
+        this.quantity = new LinkedList<>();
+    }
+
+    public Order(Long id, OrderStatus fooOrderStatus) {
+        this.id = id;
+        this.foodOrderStatus = fooOrderStatus;
+        this.foodItem = new LinkedList<>();
+        this.quantity = new LinkedList<>();
+    }
+
+    public Order(Long id, List<FoodItem> foodItem, List<Integer> quantity, OrderStatus fooOrderStatus) {
+        this.id = id;
+        this.foodItem = foodItem;
+        this.quantity = quantity;
+        this.foodOrderStatus = fooOrderStatus;
+    }
+
+    public void addFoodItem(FoodItem item, Integer quantity) {
+        this.foodItem.add(item);
+        this.quantity.add(quantity);
+    }
+    public void deleteFoodItem(FoodItem item) {
+        int quantityIndex = FoodOrder.<FoodItem>getIndexFromIterator(this.foodItem.iterator(), item);
+        this.quantity.remove(quantityIndex);
+        this.foodItem.remove(item);
+    }
+    
+    protected static <T> int getIndexFromIterator(Iterator<T> iterator, T object) {
+        int count = 0;
+        while(iterator.hasNext()) {
+            if(iterator.next().equals(object)) {
+                return count;
+            }
+            count++;
+        }
+        return -1;
+    }
 }
--- server/kdsAPI/src/main/java/com/kdsAPI/order/OrderFactory.java
+++ server/kdsAPI/src/main/java/com/kdsAPI/order/OrderFactory.java
@@ -1,14 +1,8 @@
 package com.kdsAPI.order;
-import java.util.List;
 
 import com.kdsAPI.dto.order.OrderDTO;
-import com.kdsAPI.models.FoodItem;
 
 public class OrderFactory {
-    public Order createOrder(Long id, List<FoodItem> foodItem) {
-        return new OrderDTO(id, foodItem, OrderStatus.WAITING);
-    }
-
     public Order creatOrder() {
         return new OrderDTO();
     }
