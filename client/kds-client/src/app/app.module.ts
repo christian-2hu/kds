9f6d0a943ea619c57472e9b25fb7bd1995917926
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-09 19:12:46 -0300

    [client] Install fortawesome

--- client/kds-client/src/app/app.module.ts
+++ client/kds-client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { OrdersComponent } from './components/shared/order/orders.component';
 import { OrderInfoComponent } from './components/shared/order-info/order-info.component';
 import { ButtonComponent } from './components/shared/button/button.component';
 import { OrderComponent } from './components/order/order.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,12 @@ import { OrderComponent } from './components/order/order.component';
     ButtonComponent,
     OrderComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
