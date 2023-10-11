2a422ad0283d59f2715f9242441b5b4e6c16815e
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-11 20:23:00 -0300

    Change OrderRepository's display order of findAll
    It was ordering by something else, we want it by id

--- server/kdsAPI/src/main/java/com/kdsAPI/repositories/OrderRepository.java
+++ server/kdsAPI/src/main/java/com/kdsAPI/repositories/OrderRepository.java
@@ -1,9 +1,14 @@
 package com.kdsAPI.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.kdsAPI.models.FoodOrder;
 
 public interface OrderRepository extends JpaRepository<FoodOrder, Long>{
 
+    @Query("SELECT foodOrder from FoodOrder foodOrder ORDER BY foodOrder.id ASC")
+    public List<FoodOrder> findAll();
 }
