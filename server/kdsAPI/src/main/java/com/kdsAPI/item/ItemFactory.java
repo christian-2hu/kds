472effab391748ba4dd78dada80837a11e13762a
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-11 20:31:01 -0300

    Add a recipe property on FoodItem entity
    Although not every item will have an recipe, the client needs to know some items that goes on the recipe

--- server/kdsAPI/src/main/java/com/kdsAPI/item/ItemFactory.java
+++ server/kdsAPI/src/main/java/com/kdsAPI/item/ItemFactory.java
@@ -3,8 +3,8 @@ package com.kdsAPI.item;
 import com.kdsAPI.dto.item.ItemDTO;
 
 public class ItemFactory {
-    public Item createItem(Long id, String item) {
-        return new ItemDTO(id, item);
+    public Item createItem(Long id, String item, String recipe) {
+        return new ItemDTO(id, item, recipe);
     }
 
     public Item createItem() {
