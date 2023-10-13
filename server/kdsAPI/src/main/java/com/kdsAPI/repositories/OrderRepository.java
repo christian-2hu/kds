5ecbb738791ff1ca614c8a0b7f7b05c0a0ad5c78
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-13 17:44:29 -0300

    Update OrderRepository
    findAll method now shows everything except complete orderes, and findByFoodOrderStatus can filter orders based on another status

--- server/kdsAPI/src/main/java/com/kdsAPI/repositories/OrderRepository.java
+++ server/kdsAPI/src/main/java/com/kdsAPI/repositories/OrderRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.kdsAPI.models.FoodOrder;
+import com.kdsAPI.order.OrderStatus;
 
 public interface OrderRepository extends JpaRepository<FoodOrder, Long>{
-   @Query("SELECT foodOrder from FoodOrder foodOrder ORDER BY foodOrder.id ASC")
+   @Query("SELECT foodOrder from FoodOrder foodOrder WHERE foodOrder.foodOrderStatus != OrderStatus.COMPLETE ORDER BY foodOrder.id ASC")
     public List<FoodOrder> findAll();
+
+    public List<FoodOrder> findByFoodOrderStatus(OrderStatus foodOrderStatus);
+
 }
