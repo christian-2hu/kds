5f1af91921c7b6a91121870f1a8d23443cb8519e
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-18 17:11:42 -0300

    [client] Make the sidebar more dynamic
    Menu items can be added from a parent component instead of being inside the sidebar component

--- client/kds-client/src/app/app.component.ts
+++ client/kds-client/src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  faBars,
+  faBoxArchive,
+  faBurger,
+  faGear,
+} from '@fortawesome/free-solid-svg-icons';
 import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 import {
   NavigationCancel,
@@ -8,18 +13,20 @@ import {
   NavigationStart,
   Router,
 } from '@angular/router';
+import { SidebarMenuItem } from './models/sidebar-menu-item.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild(SidebarComponent) child!: SidebarComponent;
 
   public loading: boolean = true;
   public barsIcon = faBars;
   public title = 'kds-client';
+  public sidebarMenuitems!: SidebarMenuItem[];
 
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
@@ -44,7 +51,41 @@ export class AppComponent {
     });
   }
 
+  public ngOnInit(): void {
+    this.sidebarMenuitems = this.initSidebarMenuItem();
+  }
+
   public showSidebar() {
     this.child.showSidebar();
+  }
+
+  public initSidebarMenuItem(): SidebarMenuItem[] {
+    return [
+      {
+        label: 'Orders',
+        fontAwesomeIcon: faBurger,
+        routerUrl: '/orders',
+      },
+      {
+        label: 'Archive',
+        fontAwesomeIcon: faBoxArchive,
+        routerUrl: '/archive',
+      },
+      {
+        label: 'Settings',
+        fontAwesomeIcon: faGear,
+        routerUrl: '#',
+        items: [
+          {
+            label: 'User settings',
+            routerUrl: '#',
+          },
+          {
+            label: 'Change password',
+            routerUrl: '#',
+          },
+        ],
+      },
+    ];
   }
 }
