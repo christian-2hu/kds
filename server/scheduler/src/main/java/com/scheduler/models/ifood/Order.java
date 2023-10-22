2e65bd861652c1442a453a6168b62c095c0e18e3
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-22 14:14:27 -0300

    Fix order and orderStatus package

--- server/scheduler/src/main/java/com/scheduler/models/ifood/Order.java
+++ server/scheduler/src/main/java/com/scheduler/models/ifood/Order.java
@@ -1,4 +1,4 @@
-package com.scheduler.order;
+package com.scheduler.models.ifood;
 
 import java.util.List;
 import java.util.Map;
--- server/scheduler/src/main/java/com/scheduler/models/ifood/OrderStatus.java
+++ server/scheduler/src/main/java/com/scheduler/models/ifood/OrderStatus.java
@@ -1,4 +1,4 @@
-package com.scheduler.order;
+package com.scheduler.models.ifood;
 
 public enum OrderStatus {
     WAITING,
