7cc69cf18ba52493546dc6cb5cad483d754fc4ce
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-25 13:17:10 -0300

    Add ToString annotation
    This will be only to show the string message on the logger

--- server/scheduler/src/main/java/com/scheduler/models/ifood/Order.java
+++ server/scheduler/src/main/java/com/scheduler/models/ifood/Order.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Order {
     private Long id;
     private Map<String, Integer> order;
