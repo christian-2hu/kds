7cfb2e522b2766a4fc4290523caeed328f24790d
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-26 17:57:04 -0300

    Update OrderMessageProducer

--- server/kdsAPI/src/main/java/com/kdsAPI/messaging/producers/order/OrderMessageProducer.java
+++ server/kdsAPI/src/main/java/com/kdsAPI/messaging/producers/order/OrderMessageProducer.java
@@ -3,11 +3,11 @@ package com.kdsAPI.messaging.producers.order;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
+import com.kdsAPI.dto.order.OrderDTO;
 import com.kdsAPI.messaging.producers.MessageProducer;
-import com.kdsAPI.models.order.Order;
 
 @Component
-public class OrderMessageProducer extends MessageProducer<Order>{
+public class OrderMessageProducer extends MessageProducer<OrderDTO>{
 
     public OrderMessageProducer(RabbitTemplate rabbitTemplate) {
         super(rabbitTemplate);
