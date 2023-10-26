3d91adf856bb894925a97470c11d4482b090ecba
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-26 17:27:38 -0300

    Emmit all placed orders
    On the future we will emmit canceled orders which may come from ifood(automatically or by the client)

--- server/scheduler/src/main/java/com/scheduler/scheduler/DeliveryScheduler.java
+++ server/scheduler/src/main/java/com/scheduler/scheduler/DeliveryScheduler.java
@@ -2,10 +2,12 @@ package com.scheduler.scheduler;
 
 import java.util.logging.Logger;
 
+import com.scheduler.models.ifood.Order;
+
 public abstract class DeliveryScheduler {
     protected final Logger LOGGER = Logger.getLogger(DeliveryScheduler.class.getName());
   
     public abstract void fetchOrders() throws InterruptedException;
-    public abstract void emmitOrderEvent();
+    public abstract void emmitOrderEvent(Order order);
 
 }
