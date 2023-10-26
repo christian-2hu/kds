19bbee6fb14c579af31ff7a6a75f6fb0f13501a9
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-26 20:01:08 -0300

    Save every event on OrderMessageConsumer
    These events are placed orders that come from ifood, so they can be saved without worries

--- server/kdsAPI/src/main/java/com/kdsAPI/messaging/consumers/order/OrderMessageConsumer.java
+++ server/kdsAPI/src/main/java/com/kdsAPI/messaging/consumers/order/OrderMessageConsumer.java
@@ -7,16 +7,24 @@ import org.springframework.stereotype.Component;
 
 import com.kdsAPI.dto.order.OrderDTO;
 import com.kdsAPI.messaging.consumers.MessageConsumer;
+import com.kdsAPI.models.FoodOrder;
+import com.kdsAPI.services.AbstractService;
+
+import lombok.RequiredArgsConstructor;
 
 
 
 @Component
+@RequiredArgsConstructor
 public class OrderMessageConsumer implements MessageConsumer<OrderDTO> {
     protected static final Logger LOGGER = LoggerFactory.getLogger(OrderMessageConsumer.class);
+    private final AbstractService<FoodOrder, OrderDTO> storeOrderService;
 
     @RabbitListener(queues = {"${rabbitmq.queue.order.created.name}"})
     @Override
     public void getMessage(OrderDTO message){
         LOGGER.info(String.format("Received message -> %s", message.toString()));
+        FoodOrder order = storeOrderService.save(message);
+        LOGGER.info(String.format("Order got from event was saved with id %s!", order.getId()));
     }
 }
