b18e78a30bf06ff8eaf1fcd020a2274af6ac3f46
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-10-29 19:55:49 -0300

    Add an IfoodOrderService implementation
    This implementation will be responsible for handling order messages that come from rabbitmq

---
+++ server/kdsAPI/src/main/java/com/kdsAPI/services/ifood/IfoodOrderService.java
@@ -0,0 +1,29 @@
+package com.kdsAPI.services.ifood;
+
+import org.springframework.stereotype.Service;
+
+import com.kdsAPI.dto.order.OrderDTO;
+import com.kdsAPI.models.FoodOrder;
+import com.kdsAPI.repositories.OrderRepository;
+import com.kdsAPI.services.AbstractOrderService;
+
+
+@Service
+public class IfoodOrderService extends AbstractOrderService {
+
+    public IfoodOrderService(OrderRepository repository) {
+        super(repository);
+    }
+
+    @Override 
+    public FoodOrder update(OrderDTO dto) {
+        FoodOrder updatedOrder = findByIfoodOrderId(dto.getIfoodOrderId());
+        dto.setId(updatedOrder.getId());
+        return repository.save(dto.convertToDAO());
+    }
+
+    private FoodOrder findByIfoodOrderId(String findByIfoodOrderId) {
+        OrderRepository repository = (OrderRepository) this.repository;
+        return repository.findByIfoodOrderId(findByIfoodOrderId);
+    }
+}
