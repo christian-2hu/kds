93de99c44aa1de19ea4701a232f673c250542b2c
Christian <60179743+christian-2hu@users.noreply.github.com>
2023-11-10 17:30:42 -0300

    Move the client to a different repository

--- client/kds-client/src/app/models/food-order-status.model.ts
+++
@@ -1,7 +0,0 @@
-export enum OrderStatus {
-  WAITING,
-  CONFIRMED,
-  PREPARING,
-  COMPLETE,
-  CANCELED,
-}
--- client/kds-client/src/app/models/paginated-content-response.model.ts
+++
@@ -1,7 +0,0 @@
-import { PaginationResponse } from './pagination-response.model';
-
-export interface PaginatedContentResponse<T> {
-  content: T;
-  pagination: PaginationResponse;
-  error: string | undefined;
-}
